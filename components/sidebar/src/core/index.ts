514e4276087866ed1765509d128c3093b494e73a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-07-11 23:11:47 +0900

    rename directory and export useSidebar

--- components/sidebar/src/core/index.ts
+++ components/sidebar/src/core/index.ts
@@ -1,2 +1,2 @@
-export { SidebarProvider } from "@wondesign/headless-ui/Sidebar";
+export { SidebarProvider, useSidebar } from "@wondesign/headless-ui/Sidebar";
 export type { SidebarProps as SidebarProviderProps } from "@wondesign/headless-ui/Sidebar";
